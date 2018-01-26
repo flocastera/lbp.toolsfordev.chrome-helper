7b8e589524f9991322dc897fd3b65155cab32ccc
Florian CASTERA <florian.castera@accenture.com>
2018-01-26 11:19:48 +0100

    FCA-EERMV - 26/01/2018 11:19 : Modif 'activerDV' et rajout de l'option pour l'EER

--- js/background.js
+++ js/background.js
@@ -2,25 +2,26 @@ var $ = jQuery;
 
 // Ne pas afficher le HELPER sur les pages qui ne sont pas de l'env de test
 var urlPatternsDev = ["http://localhost:*/*", "https://localhost:*/*", "https://rmoa-stmcv1.sf.intra.laposte.fr:*/*",
-	"https://imoe-stmcv1.sf.intra.laposte.fr:*/*"];
+	"https://imoe-stmcv1.sf.intra.laposte.fr:*/*", "http://imoe-stmcv1.sf.intra.laposte.fr:*/*",
+	"http://rmoa-stmcv1.sf.intra.laposte.fr:*/*"];
 var urlPatternsCCLink = ["file:///*/CC3Links-master/*"];
 var urlPatternsAnomalies = ["https://clm-prod.sf.intra.laposte.fr:*/*"];
 
 // Affiche le bouton de validation du jeu de données pour accèder à l'écran suivant
 chrome.contextMenus.create({
-	title: "Afficher bouton DC",
+	title: "Valider client (EER)",
 	"documentUrlPatterns": urlPatternsDev,
 	"contexts": ["frame", "link"],
 	onclick: function(object, tab){
 		chrome.tabs.executeScript(tab.id, {
-			file: "min_scripts/showdcbutton.js"
+			file: "min_scripts/validerclient.js"
 		});
 	} 
 });
 
 // Permet de valider le jeu de données et d'accèder automatiquement à l'écran suivant
 chrome.contextMenus.create({
-	title: "Valider DC",
+	title: "Valider DC (VTE)",
 	"documentUrlPatterns": urlPatternsDev,
 	onclick: function(object, tab){
 		chrome.tabs.executeScript(tab.id, {
--- min_scripts/validerclient.js
+++ min_scripts/validerclient.js
@@ -2,7 +2,7 @@ var test = document.getElementsByTagName('BUTTON');
 
 for( var i=0; i<test.length; ++i){
 	if( Object.keys(test[i].dataset).indexOf("adOnClick") !== -1)
-		if( test[i].dataset.adOnClick === "scope.validerDC" ){
-			test[i].style = "display: initial";
+		if( test[i].dataset.adOnClick === "scope.validerClient" ){
+			test[i].click();
 		}
 }
